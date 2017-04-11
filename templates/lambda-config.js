1c30935a9de1e4dd502e2e9992cf3f8ffe618f9c
Steve Higgs <stevejhiggs@hotmail.com>
2017-04-11 12:04:32 +0100

    Create lambda-config.js

---
+++ templates/lambda-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  region: 'eu-west-1',
+  handler: 'index.default',
+  role: 'FILLINYOURARNROLEHERE',
+  timeout: 10,
+  memorySize: 128,
+  publish: false, // default: false,
+  runtime: 'nodejs6.10'
+}
