95a09a62d911f7ed197742b9131ed56041747377
Steve Higgs <stevejhiggs@hotmail.com>
2017-06-17 14:25:06 +0100

    fix typo's in docs and examples

--- example/mynicefunction/index.ts
+++ example/mynicefunction/index.ts
@@ -1,7 +1,7 @@
-import {Handler} from '@types/aws-lambda';
+import {Handler} from 'aws-lambda';
 
 const func: Handler = (event, context, callback) => {
-  return callback(null, `key1: ${event.key1}`);  // Echo back the first key value
+  return callback(undefined, `key1: ${event.key1}`);  // Echo back the first key value
 };
 
 export default func;
